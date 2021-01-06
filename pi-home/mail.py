54f5ce24f6a721b800583da151f4631e0c396f26
killer-cc <59970366+killer-cc@users.noreply.github.com>
2021-01-06 14:02:05 +0800

    Add files via upload

---
+++ pi-home/mail.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import subprocess
+import smtplib
+import socket
+from email.mime.text import MIMEText
+import datetime
+
+# Which email address want to send
+to = '[接收的Email]'
+
+# Using specific gmail account
+gmail_user = '[發送的Email帳號]'
+gmail_password = '[密碼]'
+
+# SMTP command
+smtpserver = smtplib.SMTP('smtp.office365.com', 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+smtpserver.ehlo
+smtpserver.login(gmail_user, gmail_password)
+today = datetime.date.today()
+
+# Linux Specific shell command
+arg='ip route list'
+p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+data = p.communicate()
+split_data = data[0].split()
+ipaddr = split_data[split_data.index('src')+1]
+my_ip = 'Your ip is %s' %  ipaddr
+msg = MIMEText(my_ip)
+msg['Subject'] = 'IP For RaspberryPi on %s' % today.strftime('%b %d %Y')
+msg['From'] = gmail_user
+msg['To'] = to
+smtpserver.sendmail(gmail_user, [to], msg.as_string())
+smtpserver.quit()
---
+++ pi-home/updateV2RAY.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import urllib.request , json , time , os
+
+def getNewData():
+    #get data from database
+    url = "http://localhost/api.php?action=getDatas"
+    data = urllib.request.urlopen(url)
+    data = json.loads(data.read())
+
+    #process data
+    if data["domain"] != None:
+        domain = []
+        for i in data["domain"]:
+            domain += [i["domain"]]
+    else:
+        domain = ["example.com"]
+
+    if data["ip"] != None:
+        ip = []
+        for i in data["ip"]:
+           ip += [i["ip"]]
+    else:
+        ip = ["188.166.188.226"]
+
+    return domain,ip
+
+def update_config(domain,ip):
+    #read config file
+    v2ray_config = open("/usr/local/etc/v2ray/config.json.default","r")
+    v2ray_data = json.load(v2ray_config)
+    v2ray_config.close()
+
+    v2ray_data["routing"]["rules"][0]["domain"] = domain
+    v2ray_data["routing"]["rules"][1]["ip"] = ip
+
+    v2ray_data = json.dumps(v2ray_data,indent=2)
+
+    #write config file
+    v2ray_config = open("/usr/local/etc/v2ray/config.json","w")
+    print(v2ray_data,file=v2ray_config)
+    v2ray_config.close()
+    os.system("sudo service v2ray restart")
+
+def main():
+    domain,ip = [],[]
+    update = False
+    while True:
+        new_domain, new_ip = getNewData()
+        if new_domain != domain:
+            domain = new_domain
+            update = True
+        if new_ip != ip:
+            ip = new_ip
+            update = True
+        if update:
+            update_config(domain,ip)
+            update = False
+        time.sleep(1)
+main()
